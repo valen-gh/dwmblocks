cd58e4a7e03edf365af3506e436c413aeae6fee1
valen-gh <valen-gh@github.com>
2023-10-15 16:30:37 +0200

    Update dwmblocks

--- blocks.h
+++ blocks.h
@@ -1,13 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/			          /*Update Interval*/	/*Update Signal*/
-	{"", 		"bash $SCR/getupd", 		  3600,			        2},
+	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
+	{"", 		"bash $SCR/getupd", 		3600,			3},
 
-	{"v:", 		"bash $SCR/getvol t",		  0,			        1},
+	{"", 		"bash $SCR/getnet", 		60,			0},
 
-	{"", 		"date '+%a %d %b %H:%M'",	  60,			        0},
+	{"", 		"bash $SCR/getvol t",		0,			2},
+
+	{"", 		"bash $SCR/spsong",		1,			1},
+
+	{"", 		"bash $SCR/getdate",		60,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  ";
-static unsigned int delimLen = 5;
+static char delim[] = "   ";
+static unsigned int delimLen = 2;
